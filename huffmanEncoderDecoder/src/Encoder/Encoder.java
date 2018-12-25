09d9bfc23c6a4ef343dec0d570a1ed752464c30f
beltagy97 <43505298+beltagy97@users.noreply.github.com>
2018-12-25 02:20:48 +0200

    encoding using file as input

--- huffmanEncoderDecoder/src/Encoder/Encoder.java
+++ huffmanEncoderDecoder/src/Encoder/Encoder.java
@@ -8,25 +8,38 @@ package Encoder;
 import java.util.HashMap;
 import java.util.Map;
 import Tree.HuffmanTree;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.BitSet;
 
 /**
  *
  * @author Amr
  */
 public class Encoder {
+
     HuffmanTree tree;
     StringBuilder encodedString;
     Map<Character, Integer> freq;
+    FileOutputStream fs;
+    BitSet bits;
+    BitSet bits2;
+    byte[] data;
+    byte[] data2;
 
     /**
      *
      * @param str
      */
-    public Encoder(String str) {
+    public Encoder(String str) throws IOException {
         tree = new HuffmanTree();
         encodedString = new StringBuilder();
         freq = new HashMap<>();
-        
+        fs = new FileOutputStream(System.getProperty("user.dir") + "\\output.txt", true);
+
         for (int i = 0; i < str.length(); i++) {
             if (!freq.containsKey(str.charAt(i))) {
                 freq.put(str.charAt(i), 0);
@@ -38,15 +51,34 @@ public class Encoder {
         tree.setRoot(tree.buildTree(freq));
 
         tree.setPrefixCodes(tree.getRoot(), new StringBuilder());
-        System.out.println("Character Prefix Map = " + tree.getCharPrefixHashMap());
+        System.out.println("Character Prefix Map = ff" + tree.getCharPrefixHashMap());
 
+        for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
+           
+            String Char = Character.toString(entry.getKey());
+            data2 = Char.getBytes();
+            fs.write(data2);
+
+            //Converting correspondng huffman code to byte array to be wrriten on file for decompression
+            bits2 = fromString(tree.getCharPrefixHashMap().get(entry.getKey()));
+            data2 = bits2.toByteArray();
+            fs.write(data2);
+        }
+
+        String output = "";
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
             encodedString.append(tree.getCharPrefixHashMap().get(c));
-        }
 
+            bits = fromString(tree.getCharPrefixHashMap().get(c));
+            data = bits.toByteArray();
+
+            //System.out.println(data);
+            fs.write(data);
+        }
+        fs.close();
     }
-    
+
     public HuffmanTree getTree() {
         return tree;
     }
@@ -59,5 +91,24 @@ public class Encoder {
         return freq;
     }
 
+    private static BitSet fromString(String binary) {
+        BitSet bitset = new BitSet(binary.length());
+        int len = binary.length();
+        for (int i = len - 1; i >= 0; i--) {
+            if (binary.charAt(i) == '1') {
+                bitset.set(len - i - 1);
+            }
+        }
+        return bitset;
+    }
+    public static BitSet fromByteArray(byte[] bytes) {
+        BitSet bits = new BitSet();
+        for (int i = 0; i < bytes.length * 8; i++) {
+            if ((bytes[bytes.length - i / 8 - 1] & (1 << (i % 8))) > 0) {
+                bits.set(i);
+            }
+        }
+        return bits;
+    }
 
 }
